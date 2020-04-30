c556ee2dd8317e383bf4bcee62f5f3e35fc33a93
Charli <charlib33@gmail.com>
2020-04-30 18:11:15 +1000

    Added C# files to repo

---
+++ GameLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwinGameSDK;
+namespace MyGame.src
+{
+
+    internal static partial class GameLogic
+    {
+        public static void Main()
+        {
+            // Opens a new Graphics Window
+            SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+            // Load Resources
+            GameResources.LoadResources();
+            SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+            // Game Loop
+            do
+            {
+                GameController.HandleUserInput();
+                GameController.DrawScreen();
+            }
+            while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
+            SwinGame.StopMusic();
+
+            // Free Resources and Close Audio, to end the program.
+            GameResources.FreeResources();
+        }
+    }
+}
